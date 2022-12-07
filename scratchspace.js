13523689bbb23b55d5aff9efcb0dc92995e6807b
Michael <m.c.cotten@gmail.com>
2022-12-07 15:15:05 -0800

    changes to scratchpad

--- scratchspace.js
+++ scratchspace.js
@@ -1,3 +1,21 @@
-let array = [1, 2, 3];
-array.splice(0, 1, 'apple');
-console.log(array);
+let Person = {
+  _firstName: '',
+  _age: 35,
+  set age (age) {
+    if (typeof age === 'number') {
+      this._age = age;
+      console.log(`Ok, ${this.firstName}'s new age is ${this._age}`);
+    } else {
+      console.log("That's not a number!");
+    }
+  },
+  get firstName() {
+    if (this._firstName) {
+      return (`The person's first name is ${this._firstName}!`)
+    } else {
+      return ("This person doesn't have a name, that's weird.");
+    }
+  }
+}
+
+console.log(Person.firstName);
